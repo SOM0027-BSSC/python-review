167fd1b0dd5ad6f24973af81a9cfeb872b076782
SOM0027-BSSC <som0027@bssc.edu.au>
2025-06-19 14:58:59 +1000

    Finished task

--- 24MakeCaioSommer.py
+++ 24MakeCaioSommer.py
@@ -2,13 +2,17 @@
 # Variable Assignment
 
 # Assign your name and your favourite food to 2 separate variables with suitable names.
-
+name = "Caio"
+food = "Pizza"
 ############# SIMPLE ###################
 # Output the contents of the variables on 2 separate lines
-
+print(name)
+print(food)
 ############# MEDIUM ####################
 # Output two sentences (not just the contents of the variables). The first with your name, the second with your favourite food.
-
+print(f"My name is {name}")
+print(f"My favourite food is {food}")
 ############# COMPLEX ###################
 # Output both pieces of information as part of the same sentence.
 # Make sure that you have spaces and punctuation in the correct places. 
+print(f"My name is {name} and I like {food}.")
