2f38c6b0904c839a89cb2fe19927f0a5a3fb1fca
SOM0027-BSSC <som0027@bssc.edu.au>
2025-06-19 14:56:55 +1000

    Finished taks

--- 23modifyCaioSommer.py
+++ 23modifyCaioSommer.py
@@ -1,3 +1,6 @@
+# Caio Sommer
+# 19/06/2025
+
 # Include a comment at the top of your program with your name and date
 # Variable Assignment - Modify
 
@@ -6,15 +9,16 @@
 name1 = "Axl"
 name2 = "Slash"
 #Add 2 more variables to store 'Duff' and 'Izzy'
-
+name3 = "Duff"
+name4 = "Izzy"
 
 # The following print line uses concatenation to join variables and strings together.
 # Modify the line to join the four names variables together with an additional string
 # to create an appropriate sentance.
-print(name1 + " and " + name2 + " and ")
+print(name1 + " and " + name2 + " and " name3 + " and " + name4)
 
 # The next line produces the same output as the previous print statemtn,
 # but uses an 'f' string technique to form the final string.
 # Modify the code to produce the same output as the previous print
 # statement.
-print(f"{name1} and {name2} and ")
+print(f"{name1} and {name2} and {name3} and {name4}")
